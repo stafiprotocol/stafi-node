d0db7c1946f3b659a98918d6d5d0902883579d69
tpkeeper <tpkeeper.me@gmail.com>
2021-07-30 14:15:53 +0800

    rename to block

--- node/pallets/rdexn/signatures/src/lib.rs
+++ node/pallets/rdexn/signatures/src/lib.rs
@@ -28,9 +28,9 @@ decl_event! {
         <T as frame_system::Trait>::AccountId
     {
         /// submit signatures
-        SubmitSignatures(AccountId, RSymbol, u32, Vec<u8>, Vec<u8>),
+        SubmitSignatures(AccountId, RSymbol, u64, Vec<u8>, Vec<u8>),
         /// signatures enough
-        SignaturesEnough(RSymbol, u32, Vec<u8>),
+        SignaturesEnough(RSymbol, u64, Vec<u8>),
     }
 }
 
@@ -45,8 +45,8 @@ decl_error! {
 
 decl_storage! {
     trait Store for Module<T: Trait> as RDexnSignatures {
-        pub Signatures get(fn signatures): double_map hasher(blake2_128_concat) RSymbol, hasher(blake2_128_concat) (u32, Vec<u8>) => Option<Vec<Vec<u8>>>;
-        pub AccountSignature get(fn account_signature): map hasher(blake2_128_concat) (T::AccountId, RSymbol, u32, Vec<u8>) => Option<Vec<u8>>;
+        pub Signatures get(fn signatures): double_map hasher(blake2_128_concat) RSymbol, hasher(blake2_128_concat) (u64, Vec<u8>) => Option<Vec<Vec<u8>>>;
+        pub AccountSignature get(fn account_signature): map hasher(blake2_128_concat) (T::AccountId, RSymbol, u64, Vec<u8>) => Option<Vec<u8>>;
     }
 }
 
@@ -56,27 +56,27 @@ decl_module! {
 
         /// Submit tx signatures
         #[weight = 10_000_000]
-        pub fn submit_signatures(origin, symbol: RSymbol, era: u32, proposal_id: Vec<u8>, signature: Vec<u8>) -> DispatchResult {
+        pub fn submit_signatures(origin, symbol: RSymbol, block: u64, proposal_id: Vec<u8>, signature: Vec<u8>) -> DispatchResult {
             let who = ensure_signed(origin)?;
             ensure!(symbol.chain_type() != ChainType::Substrate, Error::<T>::InvalidRSymbol);
             ensure!(payers::Module::<T>::is_payer(symbol, &who), payers::Error::<T>::MustBePayer);
 
 
-            ensure!(Self::account_signature((&who, symbol, era, &proposal_id)).is_none(), Error::<T>::SignatureRepeated);
+            ensure!(Self::account_signature((&who, symbol, block, &proposal_id)).is_none(), Error::<T>::SignatureRepeated);
 
-            let mut signatures = Signatures::get(symbol, (era, &proposal_id)).unwrap_or(vec![]);
+            let mut signatures = Signatures::get(symbol, (block, &proposal_id)).unwrap_or(vec![]);
             ensure!(!signatures.contains(&signature), Error::<T>::SignatureRepeated);
 
             signatures.push(signature.clone());
-            Signatures::insert(symbol, (era, &proposal_id), &signatures);
+            Signatures::insert(symbol, (block, &proposal_id), &signatures);
 
-            <AccountSignature<T>>::insert((&who, symbol, era, &proposal_id), &signature);
+            <AccountSignature<T>>::insert((&who, symbol, block, &proposal_id), &signature);
 
             if signatures.len() == payers::PayerThreshold::get(symbol) as usize {
-                Self::deposit_event(RawEvent::SignaturesEnough(symbol, era, proposal_id.clone()));
+                Self::deposit_event(RawEvent::SignaturesEnough(symbol, block, proposal_id.clone()));
             }
 
-            Self::deposit_event(RawEvent::SubmitSignatures(who.clone(), symbol, era, proposal_id, signature));
+            Self::deposit_event(RawEvent::SubmitSignatures(who.clone(), symbol, block, proposal_id, signature));
             Ok(())
         }
     }
