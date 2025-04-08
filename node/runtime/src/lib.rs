844c6ffcfa72ece643c75b5744a4b21f1afe6338
Tore19 <289649077@qq.com>
2025-04-08 15:38:27 +0800

    Update min_inflation & max_inflation

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -102,7 +102,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to 0. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 29,
+	spec_version: 30,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
@@ -436,8 +436,8 @@ impl pallet_session::historical::Trait for Runtime {
 
 pallet_staking_reward_curve::build! {
 	const REWARD_CURVE: PiecewiseLinear<'static> = curve!(
-		min_inflation: 0_025_000,
-		max_inflation: 0_100_000,
+		min_inflation: 0_000_000,
+		max_inflation: 0_060_000,
 		ideal_stake: 0_750_000,
 		falloff: 0_050_000,
 		max_piece_count: 40,
