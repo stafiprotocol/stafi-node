aa64184ddfcc71cb7bc24eab49a6545a69075892
Tore19 <289649077@qq.com>
2025-04-09 10:39:31 +0800

    Update min_inflation & max_inflation

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -436,8 +436,8 @@ impl pallet_session::historical::Trait for Runtime {
 
 pallet_staking_reward_curve::build! {
 	const REWARD_CURVE: PiecewiseLinear<'static> = curve!(
-		min_inflation: 0_025_000,
-		max_inflation: 0_100_000,
+		min_inflation: 0_010_000,
+		max_inflation: 0_060_000,
 		ideal_stake: 0_750_000,
 		falloff: 0_050_000,
 		max_piece_count: 40,
